c04f7824c2e81fcb88288498ae385ac6d15458e2
Darshan Panchal <darshanpanchal804@gmail.com>
2025-07-25 00:45:02 +0530

    Remove APIKEY from Hard-code

--- src/main/java/net/Myapp/journalApp/service/WeatherService.java
+++ src/main/java/net/Myapp/journalApp/service/WeatherService.java
@@ -2,6 +2,7 @@ package net.Myapp.journalApp.service;
 
 import net.Myapp.journalApp.apiresponse.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -11,8 +12,8 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class WeatherService {
 
-
-    private static final String APIKEY="ec6e6b6260546acbe9a5c312084f1860";
+@Value("${weather.api.key}")
+    private String APIKEY;
     private static final String API= "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
 
@@ -21,7 +22,7 @@ public class WeatherService {
 
     public WeatherResponse getWeather(String city){
         String finalAPI=API.replace("CITY",city).replace("API_KEY",APIKEY);
-        System.out.println(finalAPI);
+//        System.out.println(finalAPI);
         ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
       return response.getBody();
     }
