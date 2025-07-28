4d644fdbb3efa1b3ac6c6495ccf83c59ada824a2
Darshan Panchal <darshanpanchal804@gmail.com>
2025-07-28 18:01:59 +0530

    Updated config files and added UserScheduler

---
+++ src/main/java/net/Myapp/journalApp/scheduler/UserScheduler.java
@@ -0,0 +1,17 @@
+package net.Myapp.journalApp.scheduler;
+
+import net.Myapp.journalApp.repository.UserEntryRepositoryImp;
+import net.Myapp.journalApp.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserScheduler {
+
+@Autowired
+private EmailService emailService;
+
+@Autowired
+    private UserEntryRepositoryImp userEntryRepositoryImp;
+
+//    public void fetchUserfrom
+
+}
