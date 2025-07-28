7bcabca43e3b93586473e5025ab84e3c4c0e0ea7
Darshan Panchal <darshanpanchal804@gmail.com>
2025-07-28 22:57:19 +0530

    Create Scheduler and SentimateAnalysis Service

--- src/main/java/net/Myapp/journalApp/scheduler/UserScheduler.java
+++ src/main/java/net/Myapp/journalApp/scheduler/UserScheduler.java
@@ -1,17 +1,40 @@
 package net.Myapp.journalApp.scheduler;
 
+import net.Myapp.journalApp.Entity.JournalEntry;
+import net.Myapp.journalApp.Entity.UserEntry;
 import net.Myapp.journalApp.repository.UserEntryRepositoryImp;
 import net.Myapp.journalApp.service.EmailService;
+import net.Myapp.journalApp.service.SentimentAnalysisService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserScheduler {
 
-@Autowired
-private EmailService emailService;
+    @Autowired
+    private EmailService emailService;
 
-@Autowired
+    @Autowired
     private UserEntryRepositoryImp userEntryRepositoryImp;
+    @Autowired
+    private SentimentAnalysisService sentimentAnalysisService;
 
-//    public void fetchUserfrom
+@Scheduled(cron=" * * * * *")
+    public void fetchAndSendMail() {
+        List<UserEntry> users = userEntryRepositoryImp.getUserSA();
+        for (UserEntry user : users) {
+            List<JournalEntry> journalEntries = user.getJournalEntries();
+            List<String> filteredEntries = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x->x.getContent()).collect(Collectors.toList());
+            String entry= String.join(" ",filteredEntries);
+            String sentimate = sentimentAnalysisService.getsentimate(entry);
+            emailService.sendEmail(user.getEmail(),"Sentiment for Last 7 Days ",sentimate);
+
+        }
+    }
+
 
 }
---
+++ src/main/java/net/Myapp/journalApp/service/SentimentAnalysisService.java
@@ -0,0 +1,11 @@
+package net.Myapp.journalApp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SentimentAnalysisService {
+public String getsentimate(String text){
+return " ";
+}
+
+}
